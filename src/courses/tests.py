c21aff6c46088db1249472bece4a96d97a50e7c7
Cristian Garcia <the.cristian.garcia@gmail.com>
2016-10-20 13:53:43 -0700

    Course test case: compares course attributes

--- src/courses/tests.py
+++ src/courses/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import *
+
+
 
 # Create your tests here.
+
+class CourseModelTestCase(TestCase):
+    def setUp(self):
+        self.course = Course.objects.create(course_title = 'CS408',
+                                            course_number = '1234',
+                                            course_quarter = 'Winter',
+                                            course_year = '2017')
+
+
+    def test_course_get(self):
+        stud = Course.objects.get(id = self.course.id)
+        self.assertEqual(self.course.course_title, stud.course_title)
